49fe2f36a2e875dd99b5fff73d5c1311f04e706a
Claudinei Bido <bido.c@live.com>
2020-10-12 19:00:53 +0200

    Endpoint docummentation Register as farmer or user

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,7 +5,7 @@ const { signin, login } = require('../controllers/authController');
 const { validateData } = require('../models/userValidationSchema');
 
 /* POST to register endpoint. */
-router.route('/register').post(validateData, signin);
+router.route('/register/:role').post(validateData, signin);
 
 router.route('/login').post(login);
 
