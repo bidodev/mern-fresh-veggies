7a7a3ab98175e3d6ff90641371c286e4b76715df
Kevin <kev.saintigny.schmidt@gmail.com>
2020-10-13 10:30:17 +0200

    created story folder

---
+++ client/src/components/story/Story.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Story.styles.scss';
+
+const Story = () => {
+  return (
+    <React.Fragment>
+      <section className="story">STORY SECTION</section>
+    </React.Fragment>
+  );
+};
+
+export default Story;
--- client/src/pages/landing/Landing.page.jsx
+++ client/src/pages/landing/Landing.page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Story from 'components/story/Story.component';
 
 import './Landing.styles.scss';
 
@@ -6,6 +7,7 @@ const Landing = () => {
   return (
     <React.Fragment>
       <div className="landing-page">Welcome to Landing Page!</div>
+      <Story />
     </React.Fragment>
   );
 };
