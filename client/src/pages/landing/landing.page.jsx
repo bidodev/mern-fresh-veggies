a394380bdf5e02005dafb8f89f12771b94be1723
Simona <see.moona@yahoo.com>
2020-10-13 19:17:41 +0200

    hero component added

--- client/src/pages/landing/landing.page.jsx
+++ client/src/pages/landing/landing.page.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Hero from 'components/hero/hero.component';
 import Story from 'components/story/story.component';
 import './landing.styles.scss';
@@ -7,10 +7,19 @@ import './landing.styles.scss';
 const Landing = () => {
   return (
     <React.Fragment>
-      <Hero />
-      <Story />
+      <Router>
+        <Hero />
+        <Story />
+      </Router>
     </React.Fragment>
   );
 };
 
 export default Landing;
+
+{
+  /* <Router>
+<Route to="/" component={Hero} />
+<Route to="/about" component={Story} id="about" />
+</Router> */
+}
