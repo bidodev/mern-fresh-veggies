d793c7057ed156a38ec9cc7edde2975c699a1791
Simona <see.moona@yahoo.com>
2020-10-13 18:20:08 +0200

    imported hero comp

--- client/src/pages/landing/landing.page.jsx
+++ client/src/pages/landing/landing.page.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
+import Hero from 'components/hero/hero.component';
 import Story from 'components/story/story.component';
-
 import './landing.styles.scss';
-
 
 const Landing = () => {
   return (
     <React.Fragment>
-      <div className="landing">Welcome to Landing Page!</div>
+      <Hero />
       <Story />
-
     </React.Fragment>
   );
 };
