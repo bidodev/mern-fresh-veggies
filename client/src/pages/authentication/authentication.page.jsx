2eacca4a9f0bbb522002b2e2ea0a6c9b2e1789c2
Claudinei Bido <bido.c@live.com>
2020-10-13 09:39:44 +0200

    fixs

---
+++ client/src/pages/authentication/authentication.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Authentication.styles.scss';
+
+const Authentication = () => {
+  return (
+    <React.Fragment>
+      <div className="authentication-page">Welcome to Authentication Page!</div>
+    </React.Fragment>
+  );
+};
+
+export default Authentication;
---
+++ client/src/pages/shop/shop.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Shop.styles.scss';
+
+const Shop = () => {
+  return (
+    <React.Fragment>
+      <div className="shop-page">Welcome to Shop Page!</div>
+    </React.Fragment>
+  );
+};
+
+export default Shop;
