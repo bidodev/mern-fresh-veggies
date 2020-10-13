bab51f42622929234aecd7827df81ee08f9cec35
Simona <see.moona@yahoo.com>
2020-10-13 19:17:25 +0200

    hero done

--- client/src/components/hero/hero.component.jsx
+++ client/src/components/hero/hero.component.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import './hero.styles.scss';
-
+import { HashLink as Link } from 'react-router-hash-link';
 const Hero = () => {
   return (
     <div className="hero-component">
       <header>
-        <p className="hero-component__button">About Us</p>
+        <Link
+          smooth
+          to="/about"
+          scroll={(el) =>
+            el.scrollIntoView({ behavior: 'instant', block: 'end' })
+          }
+        >
+          <p className="hero-component__button">Our Story</p>
+        </Link>
       </header>
       <div className="hero-component__main">
         <div className="hero-component__main__text-component">
