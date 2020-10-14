5879e5e456879650a12e3e098e7bc8b3fd0fa261
Kevin <kev.saintigny.schmidt@gmail.com>
2020-10-14 12:00:04 +0200

    styles is synchro

---
+++ client/src/components/footer/footer.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="placeholder">HELLO I AM FOOTER</div>
+    </footer>
+  );
+};
+
+export default Footer;
