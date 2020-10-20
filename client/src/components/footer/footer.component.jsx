8f27c32f44ac6e0a2306c25636b8def8f502e0c5
Simona <see.moona@yahoo.com>
2020-10-20 12:03:19 +0200

    footer completed

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -5,7 +5,11 @@ import './footer.styles.scss';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="placeholder">HELLO I AM FOOTER</div>
+      <div className="placeholder">
+        <p>Contact Us</p>
+        <p>FreshVeggies street, no.20, Brandenburg, Germany </p>
+        <p>+49 01522 3044584 </p>
+      </div>
     </footer>
   );
 };
