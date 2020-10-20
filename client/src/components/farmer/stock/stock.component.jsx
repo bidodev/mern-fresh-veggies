37e3505fcce5a8f3633e4aacf7c672a87ec278a6
Simona <see.moona@yahoo.com>
2020-10-20 21:32:02 +0200

    connecting with the modal

--- client/src/components/farmer/stock/stock.component.jsx
+++ client/src/components/farmer/stock/stock.component.jsx
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from 'react';
+import ReactModal from 'react-modal';
 import axios from 'axios';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import CustomButton from 'components/custom-button/custom-button.component';
 import Spinner from 'components/spinner/spinner.component';
-
+import AddForm from 'components/forms/add-product.component';
 import './stock.styles.scss';
 
-const Stock = ({jwt}) => {
+ReactModal.setAppElement('#root');
+const Stock = ({ jwt }) => {
   axios.defaults.headers.common['Authorization'] = `Bearer ${jwt}`;
 
+  /// style for the modal:
+  const customStyles = {
+    content: {
+      width: '70%',
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
   const [products, setProducts] = useState([]);
   const [fetchError, setFetchError] = useState(null);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const getProducts = async () => {
     try {
@@ -42,7 +60,26 @@ const Stock = ({jwt}) => {
             <Spinner />
           )}
         </div>
-        <CustomButton type="button">Add</CustomButton>
+        <CustomButton type="button" onClick={() => setIsOpen(!modalIsOpen)}>
+          Add
+        </CustomButton>
+
+        {modalIsOpen && (
+          <ReactModal
+            isOpen={modalIsOpen}
+            onRequestClose={() => setIsOpen(!modalIsOpen)}
+            //shouldCloseOnOverlayClick={!modalIsOpen}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <AddForm />
+            <FontAwesomeIcon
+              icon="times"
+              className="fa-times"
+              onClick={() => setIsOpen(!modalIsOpen)}
+            />
+          </ReactModal>
+        )}
       </div>
     </section>
   );
