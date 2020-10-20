e5626300b08388b68418a9d22d07b29ec794adf7
Kevin <kev.saintigny.schmidt@gmail.com>
2020-10-20 18:38:09 +0200

    moved farmer-list into components

--- client/src/pages/shop/shop.page.jsx
+++ client/src/pages/shop/shop.page.jsx
@@ -1,67 +1,27 @@
 import React, { useEffect, useState } from 'react';
-
 import { Route, Link, useParams } from 'react-router-dom';
-import axios from 'axios';
 
 /* Page Imports */
 import ProfilePage from 'pages/farmer/profile.page';
 
 /* Component Imports */
-import Spinner from 'components/spinner/spinner.component';
 import Navbar from 'components/navbar/navbar.component';
 import Feed from 'components/feed/feed.component';
+import FarmerList from 'components/farmer/farmer-list/farmer.list.component';
 import Footer from 'components/footer/footer.component';
 
 /* Styles */
 import './shop.styles.scss';
 
-const FarmerOverview = ({ match }) => {
-  const [farmers, setFarmers] = useState([]);
-  const [isLoading, setStatusLoading] = useState(true);
-
-  useEffect(() => {
-    axios(`/farmers`)
-      .then(({ data }) => {
-        setFarmers(data.data.farmers);
-        setStatusLoading(false);
-      })
-      .catch((err) => console.log(err.message));
-  }, []);
-
-  return (
-    <div>
-      <Navbar />
-      <header className="header">FREE SECTION FOR IDEAS</header>
-      <Feed />
-
-      {/* === NEED TO CREATE A NEW COMPONENT FOR FARMER-LIST === */}
-      <section className="farmer-list">
-        {/* First we load a grid with 4 farmers, then we have an option to load more */}
-        <h2>Featured Farmers</h2>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <div className="farmer-list__list">
-            {farmers.map((farmer) => (
-              <li>
-                <Link to={`${match.url}/${farmer._id}`}>{farmer.name}</Link>
-              </li>
-            ))}
-          </div>
-        )}
-      </section>
-      {/* ======== */}
-
-      <Footer />
-    </div>
-  );
-};
-
 const Shop = ({ match }) => {
   return (
     <section className="shop-page">
-      <Route exact path={`${match.path}`} component={FarmerOverview} />
+      <Navbar />
+      <div className="ideas">FREE SECTION FOR IDEAS</div>
+      <Feed />
+      <Route exact path={`${match.path}`} component={FarmerList} />
       <Route path={`${match.path}/:farmerId`} component={ProfilePage} />
+      <Footer />
     </section>
   );
 };
