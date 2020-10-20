66abbdfc63410f6a05bcd945cae814d940b40d41
Simona <see.moona@yahoo.com>
2020-10-20 12:16:37 +0200

    footer scroll working

--- client/src/components/footer/footer.component.jsx
+++ client/src/components/footer/footer.component.jsx
@@ -4,7 +4,7 @@ import './footer.styles.scss';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer" id="footer">
       <div className="placeholder">
         <p>Contact Us</p>
         <p>FreshVeggies street, no.20, Brandenburg, Germany </p>
