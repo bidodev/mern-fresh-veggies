64919adaef4a5950c8482d8eba1af111dae25e02
Simona <see.moona@yahoo.com>
2020-10-22 15:10:24 +0200

    scroll back up arrow

---
+++ client/src/components/UI/scroll.component.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './scroll.component.style.scss';
+
+const ScrollTopArrow = () => {
+  const [showScroll, setShowScroll] = useState(false);
+
+  const checkScrollTop = () => {
+    if (!showScroll && window.pageYOffset > 400) {
+      setShowScroll(true);
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  window.addEventListener('scroll', checkScrollTop);
+
+  return (
+    <FontAwesomeIcon
+      icon={['fas', 'arrow-circle-up']}
+      className="fa-arrow-circle-up"
+      onClick={scrollTop}
+      style={{ height: 40, display: showScroll ? 'flex' : 'none' }}
+    />
+  );
+};
+
+export default ScrollTopArrow;
