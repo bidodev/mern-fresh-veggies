32e982244e943b2f424e1765d04ccf9ec23e749e
Simona <see.moona@yahoo.com>
2020-10-26 20:51:23 +0100

    deleted

--- client/src/components/side-drawer/side-drawer.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import './side-drawer.styles.scss';
-
-const SideDrawer = () => {
-  return <section className="side-drawer">this is SideDrawer</section>;
-};
-
-export default SideDrawer;
