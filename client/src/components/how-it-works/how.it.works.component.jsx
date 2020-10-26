5bc5cec62c84c91a5dfbcc35fa60f90c4f3450be
Kevin <kev.saintigny.schmidt@gmail.com>
2020-10-26 19:31:50 +0100

    started info section

--- client/src/components/how-it-works/how.it.works.component.jsx
+++ client/src/components/how-it-works/how.it.works.component.jsx
@@ -38,7 +38,29 @@ const HowItWorks = () => {
       </div>
 
       {/* ======== Information Section === */}
-      <div className="how-it-works__info-section"></div>
+      <div className="how-it-works__info-section">
+        <div className="how-it-works__info-section__left-container">
+          <div className="how-it-works__info-section__left-container__text-container">
+            <h2 className="how-it-works__info-section__left-container__text-container--header">This is HEADER</h2>
+            <div className="how-it-works__info-section__left-container__text-container--text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, ullam. Alias voluptatibus aliquam itaque!
+              Impedit deleniti nihil dolorum esse ratione possimus eligendi cum ipsam itaque, aut ea quia?
+            </div>
+          </div>
+          <div className="how-it-works__info-section__left-container__img-container">
+            <img src="" alt="img" className="how-it-works__info-section__left-container__img-container--img" />
+          </div>
+        </div>
+
+        <div className="how-it-works__info-section__right-container">
+          <div className="how-it-works__info-section__right-container__img-container-1">
+            <img src="" alt="img" className="how-it-works__info-section__right-container__img-container-1--img" />
+          </div>
+          <div className="how-it-works__info-section__right-container__img-container-2">
+            <img src="" alt="img" className="how-it-works__info-section__right-container__img-container-2--img" />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
