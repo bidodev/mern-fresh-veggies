7303165f53fc29961ca966c3b69f6d18d3ed1def
Simona <see.moona@yahoo.com>
2020-10-27 11:43:42 +0100

    new props added

--- client/src/components/modal/modal.component.jsx
+++ client/src/components/modal/modal.component.jsx
@@ -9,6 +9,16 @@ const ShowModal = ({ modalStatus, closeModal, children, styles, className, overl
     <Modal
       isOpen={modalStatus}
       onRequestClose={closeModal}
+      shouldCloseOnOverlayClick={
+        true
+        /* Boolean indicating if the overlay should close the modal */
+      }
+      shouldCloseOnEsc={
+        true
+        /* Boolean indicating if pressing the esc key should close the modal
+         Note: By disabling the esc key from closing the modal
+         you may introduce an accessibility issue. */
+      }
       style={styles}
       className={className}
       overlayClassName={overlayClassName}
