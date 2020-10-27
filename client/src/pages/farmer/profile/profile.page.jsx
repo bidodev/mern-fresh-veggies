39b29a7635c176c66be8520075e29f22e24e1966
Simona <see.moona@yahoo.com>
2020-10-27 11:45:42 +0100

    cart modal opens on click

--- client/src/pages/farmer/profile/profile.page.jsx
+++ client/src/pages/farmer/profile/profile.page.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 import axios from 'axios';
 
 /* Component Imports */
@@ -8,6 +9,7 @@ import Navbar from 'components/navbar/customer-navbar.component';
 import Profile from 'components/profile/profile.component';
 import Recipes from 'components/recipes/recipes.component';
 import Footer from 'components/footer/footer.component';
+import Modal from 'components/modal/modal.component';
 
 /* Styles */
 import './profile.page.styles.scss';
@@ -31,29 +33,42 @@ const PhotosGallery = () => {
 
 /* Component Farmer Products */
 const FarmerProducts = ({ farmer }) => {
+  const modalStatus = useSelector((state) => state.status.modal);
+  console.log(modalStatus);
+  const dispatch = useDispatch();
   return (
-    <section className="profile-page">
-      <h2 className="profile-page--header">{farmer.name}'s available products</h2>
-      {
-        <div className="profile-page__farmer-products">
-          {farmer.products.map((product) => (
-            <div className="profile-page__farmer-products__card">
-              <h3 className="profile-page__farmer-products__card--name">{product.name}</h3>
-              <div className="profile-page__farmer-products__card__img-container">
-                <img
-                  src={`/images/users/${product.photo}`}
-                  alt="img"
-                  className="profile-page__farmer-products__card__img-container--img"
-                />
+    <React.Fragment>
+      <Modal
+        modalStatus={modalStatus}
+        closeModal={() => dispatch({ type: 'TOGGLE_MODAL' })}
+        className="Modal"
+        overlayClassName="Overlay"
+      >
+        This is what you added to cart
+      </Modal>
+      <section className="profile-page">
+        <h2 className="profile-page--header">{farmer.name}'s available products</h2>
+        {
+          <div className="profile-page__farmer-products">
+            {farmer.products.map((product) => (
+              <div className="profile-page__farmer-products__card">
+                <h3 className="profile-page__farmer-products__card--name">{product.name}</h3>
+                <div className="profile-page__farmer-products__card__img-container">
+                  <img
+                    src={`/images/users/${product.photo}`}
+                    alt="img"
+                    className="profile-page__farmer-products__card__img-container--img"
+                  />
+                </div>
+                <span className="profile-page__farmer-products__card--type">Type: {product.type}</span>
+                <p className="profile-page__farmer-products__card--description">Information: {product.description}</p>
               </div>
-              <span className="profile-page__farmer-products__card--type">Type: {product.type}</span>
-              <p className="profile-page__farmer-products__card--description">Information: {product.description}</p>
-            </div>
-          ))}
-        </div>
-      }
-      {/* query the specific farmer and show the profile */}
-    </section>
+            ))}
+          </div>
+        }
+        {/* query the specific farmer and show the profile */}
+      </section>
+    </React.Fragment>
   );
 };
 
