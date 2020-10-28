58fbba5001cd30b3e99ab47cc5e8afe0c650a517
Simona <see.moona@yahoo.com>
2020-10-28 14:14:32 +0100

    client sign in and out working

--- client/src/components/navbar/customer-navbar.component.jsx
+++ client/src/components/navbar/customer-navbar.component.jsx
@@ -3,20 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 /* Styles */
 import './customer-navbar.styles.scss';
 
 const NavigationCustomer = () => {
   //we are grabbing the login state and if it is true, we change the navbar to the account icon
-  const isLoggedIn = useSelector((state) => state.status.login);
-  console.log(isLoggedIn);
+  const isLoggedIn = useSelector((state) => state.login.farmerUser);
   const dispatch = useDispatch();
-  const history = useHistory();
-  const reloadPageHandler = () => {
-    dispatch({ type: 'LOGIN_USER' });
-    window.location.reload(false);
-  };
+
   return (
     <nav className="customer-navbar">
       <Link to="/">
@@ -47,7 +41,7 @@ const NavigationCustomer = () => {
             </li>
             <li>
               <Link to="#">
-                <div className="customer-navbar__account__logout" onClick={() => reloadPageHandler()}>
+                <div className="customer-navbar__account__logout" onClick={() => dispatch({ type: 'LOGOUT_USER' })}>
                   LOG OUT
                 </div>
               </Link>
@@ -55,9 +49,14 @@ const NavigationCustomer = () => {
           </React.Fragment>
         ) : (
           <li>
-            <div className="customer-navbar__account__login" onClick={() => dispatch({ type: 'TOGGLE_SIGN-IN_MODAL' })}>
-              SIGN IN
-            </div>
+            <Link to="#">
+              <div
+                className="customer-navbar__account__login"
+                onClick={() => dispatch({ type: 'TOGGLE_SIGN-IN_MODAL' })}
+              >
+                SIGN IN
+              </div>
+            </Link>
           </li>
         )}
       </ul>
