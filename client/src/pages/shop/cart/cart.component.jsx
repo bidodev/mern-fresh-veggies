9897a9aa8c9e76540c378ffc63940b3b88e79817
Simona <see.moona@yahoo.com>
2020-11-05 10:18:27 +0100

    new link to checkout

--- client/src/pages/shop/cart/cart.component.jsx
+++ client/src/pages/shop/cart/cart.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
 import CustomButton from 'components/UI/custom-button/custom-button.component';
@@ -25,17 +25,11 @@ const Cart = () => {
           <li>Your cart is empty</li>
         )}
       </ul>
-      <Router>
-        {/* <Link to="/checkout"> */}
-        <Route path="/checkout" component={Checkout}>
-          <CustomButton>
-            {/* <Link to="/checkout"> */}
-            Proceed to checkout
-            {/* </Link> */}
-          </CustomButton>
-        </Route>
-      </Router>
-      {/* </Link> */}
+
+      <Link to="/checkout">
+        <CustomButton>Proceed to checkout</CustomButton>
+      </Link>
+      <Route exact path={'/checkout'} component={Checkout}></Route>
     </div>
   );
 };
