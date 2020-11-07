bb2898127cdaffdcdcdf20d7a94993816e79b7ca
Kevin <kev.saintigny.schmidt@gmail.com>
2020-11-07 17:41:01 +0100

    banner

--- client/src/pages/shop/overview/banner/banner.component.jsx
+++ client/src/pages/shop/overview/banner/banner.component.jsx
@@ -5,8 +5,20 @@ import './banner.styles.scss';
 
 const Banner = () => {
   return (
-    <section className="overview-banner">
-      <h2 className="overview-banner__header">BANNER</h2>
+    <section className="shop-overview">
+      <div className="shop-overview__banner-container">
+        <img src="/images/banner2.jpg" alt="banner-img" className="shop-overview__banner-container--img" />
+        <h2 className="shop-overview__banner-container--header">FRESH FROM THE SOURCE</h2>
+      </div>
+      <div className="shop-overview__text-container">
+        <h3 className="shop-overview__text-container--header">OUR PROMISE</h3>
+        <div className="shop-overview__text-container--text">
+          At Veggies, we focus on every step of our ingredients journeys. From field to cutting board, our culinary and
+          sourcing teams of farmers work tirelessly to ensure the entire recipe development process is as
+          environmentally friendly, sustainable, and delicious as possible. For us, where our food comes from is as
+          important as where it's going.
+        </div>
+      </div>
     </section>
   );
 };
