1577318d748840378cbdd25ed4a5a37552b4b0fb
Simona <see.moona@yahoo.com>
2020-11-10 16:29:02 +0100

    pay card working

--- client/src/pages/checkout/checkout.page.jsx
+++ client/src/pages/checkout/checkout.page.jsx
@@ -5,7 +5,7 @@ import 'react-credit-cards/lib/styles.scss';
 
 const Checkout = () => {
   const [number, setNumber] = useState('');
-  const [displayCVC, setDisplayCVC] = useState('');
+  const [displayCVC, setDisplayCVC] = useState('3243');
   const [expiryDate, setExpiryDate] = useState('');
   const [focused, setFocused] = useState('');
   const [name, setName] = useState('');
@@ -14,12 +14,36 @@ const Checkout = () => {
   };
   console.log(name);
   const handleInputChange = (e) => {
+    console.log(e.target);
     const { name, value } = e.target;
-    setName(value);
-    setNumber(value);
+    if (name === 'number') {
+      setNumber(value);
+    } else if (name === 'name') {
+      setName(value);
+    } else if (name === 'expiry') {
+      setExpiryDate(value);
+    } else if (name === 'cvc') {
+      setDisplayCVC(value);
+    }
   };
+
+  //error on switch cases
+  // const handleInputChange = (e) => {
+  //   const { name, value } = e.target;
+  //   switch (name) {
+  //     case 'number':
+  //       setNumber(value);
+  //     case 'name':
+  //       setName(value);
+  //     case 'expiry':
+  //       setExpiryDate(value);
+  //     case 'cvc':
+  //       setDisplayCVC(value);
+  //     default:
+  //   }
+  // };
   return (
-    <div id="PaymentForm">
+    <div className="PaymentForm">
       <Cards cvc={displayCVC} expiry={expiryDate} focused={focused} name={name} number={number} />
       <form>
         <div className="form-group">
