85b5765af2db9e4ee721d444c36c8912a0480aed
Kevin <kev.saintigny.schmidt@gmail.com>
2020-11-11 23:10:13 +0100

    fixed infinit scroll bar

--- client/src/pages/shop/overview/find.farmer/find.farmer.component.jsx
+++ client/src/pages/shop/overview/find.farmer/find.farmer.component.jsx
@@ -9,6 +9,88 @@ import './find.farmer.styles.scss';
 /* Component Imports */
 import Spinner from 'components/UI/spinner/spinner.component';
 
+class FindYourFarmer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      hasMore: true,
+      slicedItems: [],
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    axios(`/farmers`)
+      .then(({ data }) => {
+        const slicedItems = data.data.farmers.slice(0, 3);
+        this.setState({ data: data.data.farmers, slicedItems });
+        this.setState({ isLoading: false });
+      })
+      .catch((err) => console.log(err.message));
+  }
+
+  fetchMoreData = () => {
+    const { data, slicedItems } = this.state;
+    if (slicedItems.length >= data.length) {
+      this.setState({ hasMore: false });
+      return;
+    }
+    setTimeout(() => {
+      const newItems = data.slice(slicedItems.length, slicedItems.length + 3);
+      this.setState({ slicedItems: slicedItems.concat(newItems) });
+    }, 1300);
+  };
+
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <section className="find-farmer">
+        <h2 className="find-farmer__header">MEET OUR VENDORS</h2>
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <div className="find-farmer__container">
+            <InfiniteScroll
+              dataLength={this.state.slicedItems.length}
+              next={this.fetchMoreData}
+              hasMore={this.state.hasMore}
+              loader={<Spinner />}
+              height="35.8rem"
+              className="find-farmer__container__infinite-scroll-bar"
+            >
+              {this.state.slicedItems.map((farmer) => (
+                <Link key={farmer._id} to={`${this.props.match.url}/farmer/${farmer._id}`}>
+                  <li className="find-farmer__container__item">
+                    <div className="find-farmer__container__item__bg-image-container">
+                      <img src="/images/farm-1.jpg" alt="background" />
+                    </div>
+                    <div className="find-farmer__container__item__avatar-container">
+                      <img
+                        src={`/uploads/${farmer.name.toLowerCase()}/images/profile/${farmer.images.profile}`}
+                        alt="farmer-avatar"
+                      />
+                    </div>
+                    <h3 className="find-farmer__container__item--name">{farmer.name}</h3>
+                    <div className="find-farmer__container__item__location">
+                      <div className="find-farmer__container__item__location--city">Berlin</div>
+                      <div className="find-farmer__container__item__location--country">Germany</div>
+                    </div>
+                    <div className="find-farmer__container__item--description">{farmer.description}</div>
+                  </li>
+                </Link>
+              ))}
+            </InfiniteScroll>
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default FindYourFarmer;
+
 // const FindYourFarmer = ({ match }) => {
 //   const [isLoading, setStatusLoading] = useState(true);
 //   const [farmers, setFarmers] = useState([]);
@@ -57,91 +139,3 @@ import Spinner from 'components/UI/spinner/spinner.component';
 //     </section>
 //   );
 // };
-
-class FindYourFarmer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isLoading: true,
-      hasMore: true,
-      slicedItems: [],
-      data: [],
-    };
-  }
-
-  componentDidMount() {
-    axios(`/farmers`)
-      .then(({ data }) => {
-        const slicedItems = data.data.farmers.slice(0, 3);
-        console.log(data.data.farmers);
-        console.log(slicedItems);
-
-        this.setState({ data: data.data.farmers, slicedItems });
-        this.setState({ isLoading: false });
-      })
-      .catch((err) => console.log(err.message));
-  }
-
-  fetchMoreData = () => {
-    const { data, slicedItems } = this.state;
-    console.log(data.length, slicedItems.length);
-
-    if (slicedItems.length >= data.length) {
-      this.setState({ hasMore: false });
-      return;
-    }
-
-    setTimeout(() => {
-      const newItems = data.slice(slicedItems.length, slicedItems.length + 9);
-      this.setState({ slicedItems: slicedItems.concat(newItems) });
-    }, 1300);
-  };
-
-  render() {
-    const { isLoading } = this.state;
-    return (
-      <section className="find-farmer">
-        <h2 className="find-farmer__header">MEET OUR VENDORS</h2>
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <div className="find-farmer__container">
-            <InfiniteScroll
-              dataLength={this.state.data.length}
-              next={this.fetchMoreData}
-              hasMore={this.state.hasMore}
-              loader={<div className="loader-fix"></div>}
-              height="35.8rem"
-              className="find-farmer__container__infinite-scroll-bar"
-            >
-              {this.state.slicedItems.map((farmer) => (
-                <Link key={farmer._id} to={`${this.props.match.url}/farmer/${farmer._id}`}>
-                  <li className="find-farmer__container__item">
-                    <div className="find-farmer__container__item__bg-image-container">
-                      <img src="/images/farm-1.jpg" alt="background" />
-                    </div>
-                    <div className="find-farmer__container__item__avatar-container">
-                      <img
-                        src={`/uploads/${farmer.name.toLowerCase()}/images/profile/${farmer.images.profile}`}
-                        alt="farmer-avatar"
-                      />
-                    </div>
-                    <h3 className="find-farmer__container__item--name">{farmer.name}</h3>
-                    <div className="find-farmer__container__item__location">
-                      <div className="find-farmer__container__item__location--city">Berlin</div>
-                      <div className="find-farmer__container__item__location--country">Germany</div>
-                    </div>
-                    <div className="find-farmer__container__item--description">{farmer.description}</div>
-                  </li>
-                </Link>
-              ))}
-            </InfiniteScroll>
-          </div>
-        )}
-      </section>
-    );
-  }
-}
-
-export default FindYourFarmer;
