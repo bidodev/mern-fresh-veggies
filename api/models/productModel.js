83f2ca1e0da810af10252360cad5fd4fef89be47
Claudinei Bido <bido.c@live.com>
2020-11-16 11:57:32 +0100

    update database and add product

--- api/models/productModel.js
+++ api/models/productModel.js
@@ -3,11 +3,31 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   owner: { type: mongoose.Schema.ObjectId, ref: 'User' },
   name: {
-      type: String,
+    type: String,
+    required: [true, 'The product name is required'],
     },
   type: String,
   description: {
     type: String,
+    default: 'Please edit the product description'
+  },
+  price: {
+    type: Number,
+    required: [true, 'The price is required']
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'The amount is required'],
+    default: 1
+  },
+  unity: {
+    type: String,
+    required: [true, 'The amount is required'],
+    default: 'unity'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
   },
   photo: {
     type: String,
