945fe9bceb205b37b6efbcc3b9e442b235cf491f
Claudinei Bido <bido.c@live.com>
2020-11-20 19:14:48 +0100

    fixed nagivation bug when navigating between components

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux';
 
 /* React Router Dom */
 import { Route, Switch } from 'react-router-dom';
+import ScrollIntoView from 'components/UI/ScrollIntoView';
 
 /* Page Imports */
 import Landing from 'pages/landing/landing.page';
@@ -21,26 +22,28 @@ const App = () => {
 
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/shop" render={({ match }) => <Shop match={match} />} />
-        <Route path="/checkout" render={({ match }) => <Checkout match={match} />} />
-        <Route path="/success" render={({ match }) => <SuccessAnimation match={match} />} />
-        <Route
-          path="/farmer/admin"
-          render={({ match }) =>
-            /* 
+      <ScrollIntoView>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/shop" render={({ match }) => <Shop match={match} />} />
+          <Route path="/checkout" render={({ match }) => <Checkout match={match} />} />
+          <Route path="/success" render={({ match }) => <SuccessAnimation match={match} />} />
+          <Route
+            path="/farmer/admin"
+            render={({ match }) =>
+              /* 
             <FarmerAdmin/> is displayed only when the farmer is logged
             While using the render Method, you can pass other props besides match such as location
             */
-            currentUser && currentUser.data.role === 'farmer' ? (
-              <FarmerAdmin match={match} />
-            ) : (
-              <PreAdminPanel match={match} />
-            )
-          }
-        />
-      </Switch>
+              currentUser && currentUser.data.role === 'farmer' ? (
+                <FarmerAdmin match={match} />
+              ) : (
+                <PreAdminPanel match={match} />
+              )
+            }
+          />
+        </Switch>
+      </ScrollIntoView>
     </>
   );
 };
