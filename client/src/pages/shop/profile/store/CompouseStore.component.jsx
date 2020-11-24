528c1cdb0e644dc9ccb8f32ad61ca3a2929b65fd
Claudinei Bido <bido.c@live.com>
2020-11-24 14:31:46 +0100

    commit

---
+++ client/src/pages/shop/profile/store/CompouseStore.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/* Import themes */
+import THEMES from 'settings/public-profile/Themes';
+
+/* Component Imports */
+import OpenStore from 'pages/shop/profile/store/OpenStore.component';
+import ClosedStore from 'pages/shop/profile/store/ClosedStore.component';
+
+const CompouseStore = ({ farmer }) => {
+  const { config } = farmer;
+
+  return (
+    <div className={`public-profile__wrapper ${config.color ? THEMES[config.color] : THEMES['default']}`}>
+      {config.open ? <OpenStore farmer={farmer} /> : <ClosedStore />}
+    </div>
+  );
+};
+
+export default CompouseStore;
