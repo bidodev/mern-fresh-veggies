93ff9e1b625221a189d5e13557f993d2379d6b67
Claudinei Bido <bido.c@live.com>
2020-12-19 17:30:44 +0100

    Avoid auto re render of app.js on each login

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import React, { Suspense } from 'react';
 
-/* Redux */
-import { useSelector } from 'react-redux';
-
 /* React Router Dom */
 import { Route, Switch } from 'react-router-dom';
 
@@ -12,7 +9,6 @@ import ScrollIntoView from 'components/UI/ScrollIntoView';
 /* Page Imports */
 const Landing = React.lazy(() => import('pages/landing/landing.page'));
 const PreAdminPanel = React.lazy(() => import('pages/preadminpanel/wrapper'));
-const FarmerAdmin = React.lazy(() => import('pages/farmeradmin/wrapper'));
 const Shop = React.lazy(() => import('pages/shop/shop.page'));
 
 //import Checkout from 'pages/checkout/checkout.page';
@@ -21,7 +17,6 @@ const Shop = React.lazy(() => import('pages/shop/shop.page'));
 /* App wrapper */
 const App = () => {
   /* Pull out the userFarmer from the redux state */
-  const currentUser = useSelector(({ login }) => login.user);
 
   return (
     <>
@@ -30,22 +25,10 @@ const App = () => {
           <Switch>
             <Route exact path="/" component={Landing} />
             <Route path="/shop" render={({ match }) => <Shop match={match} />} />
+            <Route path="/farmer/admin" render={({ match }) => <PreAdminPanel match={match} />} />
+
             {/* <Route path="/checkout" render={({ match }) => <Checkout match={match} />} /> */}
             {/* <Route path="/success" render={({ match }) => <SuccessAnimation match={match} />} /> */}
-            <Route
-              path="/farmer/admin"
-              render={({ match }) =>
-                /* 
-            <FarmerAdmin/> is displayed only when the farmer is logged
-            While using the render Method, you can pass other props besides match such as location
-            */
-                currentUser && currentUser.data.role === 'farmer' ? (
-                  <FarmerAdmin match={match} />
-                ) : (
-                  <PreAdminPanel match={match} />
-                )
-              }
-            />
           </Switch>
         </Suspense>
       </ScrollIntoView>
