c03203fa8a2a81487c39300aeb1e726e83616065
Claudinei Bido <claudinei.bido@extern.dkbcodefactory.com>
2021-09-11 13:55:20 +0200

    feat(VEGG-001): convert app to use typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,13 +11,8 @@ const Landing = React.lazy(() => import('pages/landing/landing.page'));
 const PreAdminPanel = React.lazy(() => import('pages/preadminpanel/wrapper'));
 const Shop = React.lazy(() => import('pages/shop/shop.page'));
 
-//import Checkout from 'pages/checkout/checkout.page';
-//import SuccessAnimation from 'components/UI/success/success.component';
-
 /* App wrapper */
-const App = () => {
-  /* Pull out the userFarmer from the redux state */
-
+const App: React.FC = () => {
   return (
     <>
       <ScrollIntoView>
@@ -26,9 +21,6 @@ const App = () => {
             <Route exact path="/" component={Landing} />
             <Route path="/shop" render={({ match }) => <Shop match={match} />} />
             <Route path="/farmer/admin" render={({ match }) => <PreAdminPanel match={match} />} />
-
-            {/* <Route path="/checkout" render={({ match }) => <Checkout match={match} />} /> */}
-            {/* <Route path="/success" render={({ match }) => <SuccessAnimation match={match} />} /> */}
           </Switch>
         </Suspense>
       </ScrollIntoView>
